9530cbc56e1a876905e14ea6552b208531f735a5
Vinh <vinh@axcoto.com>
2020-03-30 11:08:37 -0700

    Move code generation into detail screen

--- render/code.go
+++ render/code.go
@@ -16,6 +16,77 @@ import (
 	"github.com/yeo/bima/dto"
 )
 
+func DrawViewCode(bima *bima.Bima, token *dto.Token) *widget.Button {
+	button := widget.NewButton("View", func() {
+		w := bima.UI.Window
+
+		otpCode, _ := totp.GenerateCode(token.DecryptToken(bima.Registry.MasterPassword), time.Now())
+		otpLbl := canvas.NewText(otpCode, color.RGBA{135, 0, 16, 255})
+		otpLbl.TextSize = 20
+
+		done := make(chan bool)
+		go func() {
+			//secs := time.Now().Unix()
+			//remainder := secs % 30
+			//time.Sleep(time.Duration(30-remainder) * time.Second)
+			ticker := time.NewTicker(30 * time.Second)
+			for {
+				select {
+				case v := <-done:
+					if v {
+						log.Debug().Msg("Back to main screen")
+						return
+					}
+				case <-ticker.C:
+					otpCode, _ := totp.GenerateCode(token.DecryptToken(bima.Registry.MasterPassword), time.Now())
+					otpLbl.Text = otpCode
+					otpLbl.Refresh()
+					log.Debug().Msg("Refresh token")
+				}
+			}
+		}()
+
+		var btn *widget.Button
+		btn = widget.NewButton("Copy", func() {
+			w.Clipboard().SetContent(otpCode)
+			btn.SetText("Copied")
+			btn.Style = widget.PrimaryButton
+			timer2 := time.NewTimer(time.Second * 10)
+			go func() {
+				<-timer2.C
+				if btn != nil {
+					btn.SetText("Copy")
+				}
+			}()
+		})
+
+		actionButtons := widget.NewHBox(
+			btn,
+			widget.NewButton("Back", func() {
+				done <- true
+				bima.UI.Window.SetContent(bima.UI.MainContainer)
+				DrawCode(bima)
+			}),
+		)
+
+		container := fyne.NewContainerWithLayout(layout.NewGridLayout(1),
+			widget.NewVBox(
+				canvas.NewText(token.URL, color.RGBA{135, 0, 16, 255}),
+				canvas.NewText(token.Name, color.RGBA{135, 0, 16, 255}),
+				otpLbl,
+			),
+			layout.NewSpacer(),
+			actionButtons,
+			layout.NewSpacer(),
+		)
+
+		bima.UI.Window.SetContent(container)
+		container.Refresh()
+	})
+
+	return button
+}
+
 func DrawCode(bima *bima.Bima) {
 	bima.AppModel.CurrentScreen = "token/list"
 	w := bima.UI.Window
@@ -25,7 +96,6 @@ func DrawCode(bima *bima.Bima) {
 	codeContainer := fyne.NewContainerWithLayout(layout.NewGridLayout(1))
 	if err == nil {
 		for _, token := range tokens {
-			otpCode, _ := totp.GenerateCode(token.DecryptToken(bima.Registry.MasterPassword), time.Now())
 
 			if bima.AppModel.FilterText != "" {
 				if !strings.Contains(token.Name, bima.AppModel.FilterText) &&
@@ -34,37 +104,16 @@ func DrawCode(bima *bima.Bima) {
 					continue
 				}
 			}
-
-			otpLbl := canvas.NewText(otpCode, color.RGBA{135, 0, 16, 255})
-			otpLbl.TextSize = 20
-
-			var btn *widget.Button
-			btn = widget.NewButton("Copy", func() {
-				w.Clipboard().SetContent(otpCode)
-				btn.SetText("Copied")
-				btn.Style = widget.PrimaryButton
-				timer2 := time.NewTimer(time.Second * 10)
-				go func() {
-					<-timer2.C
-					if btn != nil {
-						btn.SetText("Copy")
-					}
-				}()
-			})
-
+			viewButton := DrawViewCode(bima, token)
 			editButton := DrawEditCode(bima, token)
+
 			row :=
 				fyne.NewContainerWithLayout(layout.NewGridLayout(1),
-					widget.NewGroup(token.Name+":"+token.URL,
+					widget.NewGroup(token.URL,
 						widget.NewHBox(
+							canvas.NewText(token.Name, color.RGBA{135, 0, 16, 255}),
 							layout.NewSpacer(),
-							otpLbl,
-							layout.NewSpacer(),
-						),
-						widget.NewHBox(
-							layout.NewSpacer(),
-							btn,
-							layout.NewSpacer(),
+							viewButton,
 							editButton,
 						),
 					),
