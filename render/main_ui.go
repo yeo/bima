638fa6a2604268e9e4b83006376fe9b01ffc3c38
Vinh <vinh@axcoto.com>
2020-04-17 00:54:01 -0700

    ceanup dead code

--- render/main_ui.go
+++ render/main_ui.go
@@ -6,29 +6,6 @@ import (
 	"github.com/yeo/bima/core"
 )
 
-type OtpRefresher struct {
-	OtpSecret []string
-}
-
-func (r *OtpRefresher) Start() {
-	//go func() {
-	//	secs := time.Now().Unix()
-	//	remainder := secs % 30
-	//	time.Sleep(time.Duration(30-remainder) * time.Second)
-	//	ticker := time.NewTicker(30 * time.Second)
-	//	for {
-	//		select {
-	//		case <-ticker.C:
-	//			if bima.AppModel.CurrentScreen == "token/list" {
-	//				DrawCode(bima)
-	//			}
-	//		}
-	//	}
-	//}()
-}
-func (r *OtpRefresher) Stop() {
-}
-
 func DrawMainUI(bima *bima.Bima) {
 	bima.AppModel.CurrentScreen = "token/list"
 	DrawCode(bima)
