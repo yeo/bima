a5e140ad3e288f977f46b511a70a79b4966c4c74
Vinh <vinh@axcoto.com>
2020-04-18 01:13:53 -0700

    Move all ui code to render

--- cmd/bima/main.go
+++ cmd/bima/main.go
@@ -3,12 +3,8 @@ package main
 import (
 	"os"
 
-	"fyne.io/fyne"
 	"fyne.io/fyne/app"
-	//"fyne.io/fyne/canvas"
-	"fyne.io/fyne/layout"
 	"fyne.io/fyne/theme"
-	"fyne.io/fyne/widget"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -21,7 +17,6 @@ import (
 
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	if debugFlag := os.Getenv("DEBUG"); debugFlag == "1" {
 		zerolog.SetGlobalLevel(zerolog.DebugLevel)
@@ -29,7 +24,6 @@ func main() {
 
 	a := app.New()
 	a.Settings().SetTheme(theme.LightTheme())
-
 	w := a.NewWindow("Bima")
 
 	dbCon, err := db.Setup()
@@ -39,38 +33,7 @@ func main() {
 	dto.SetDB(dbCon)
 
 	bima := bima.New(w, dbCon)
-
-	searchBox := &widget.Entry{
-		PlaceHolder: "Search",
-		MultiLine:   false,
-		OnChanged: func(t string) {
-			bima.AppModel.FilterText = t
-			render.DrawCode(bima)
-		},
-	}
-
-	addButton := render.DrawNewCode(bima)
-	settingButton := render.DrawSetting(bima)
-
-	headerWidget := widget.NewHBox(searchBox, layout.NewSpacer(), addButton, settingButton)
-
-	header := fyne.NewContainerWithLayout(layout.NewFixedGridLayout(fyne.Size{300, 50}), headerWidget)
-	bima.UI.Header = header
-
-	// To avoid the annoying of password when debugging, we support set password via env.
-	if password := os.Getenv("BIMAPASS"); password != "" {
-		bima.Registry.MasterPassword = password
-	}
-
-	if bima.Registry.MasterPassword == "" {
-		render.DrawMasterPassword(bima, render.DrawMainUI)
-	} else {
-		render.DrawMainUI(bima)
-	}
-	go bima.Sync.Watch()
-
-	w.Resize(fyne.NewSize(320, 640))
-	w.ShowAndRun()
+	render.Render(bima)
 
 	cleanup(bima)
 }
--- render/main_ui.go
+++ render/main_ui.go
@@ -1,12 +1,58 @@
 package render
 
 import (
+	"os"
 	//"time"
+
+	"fyne.io/fyne"
+	"fyne.io/fyne/layout"
+	"fyne.io/fyne/widget"
 
 	"github.com/yeo/bima/core"
 )
 
+func AddHeader(bima *bima.Bima) {
+	searchBox := &widget.Entry{
+		PlaceHolder: "Search",
+		MultiLine:   false,
+		OnChanged: func(t string) {
+			bima.AppModel.FilterText = t
+			DrawCode(bima)
+		},
+	}
+
+	addButton := DrawNewCode(bima)
+	settingButton := DrawSetting(bima)
+
+	headerWidget := widget.NewHBox(searchBox, layout.NewSpacer(), addButton, settingButton)
+
+	header := fyne.NewContainerWithLayout(layout.NewFixedGridLayout(fyne.Size{300, 50}), headerWidget)
+
+	bima.UI.Header = header
+}
+
 func DrawMainUI(bima *bima.Bima) {
 	bima.AppModel.CurrentScreen = "token/list"
 	DrawCode(bima)
+}
+
+// main Entrypoint to render whole ui
+func Render(bima *bima.Bima) {
+	AddHeader(bima)
+
+	// To avoid the annoying of password when debugging, we support set password via env.
+	if password := os.Getenv("BIMAPASS"); password != "" {
+		bima.Registry.MasterPassword = password
+	}
+
+	if bima.Registry.MasterPassword == "" {
+		DrawMasterPassword(bima, DrawMainUI)
+	} else {
+		DrawMainUI(bima)
+	}
+
+	go bima.Sync.Watch()
+
+	bima.UI.Window.Resize(fyne.NewSize(320, 640))
+	bima.UI.Window.ShowAndRun()
 }
