9d54f1875e3aa882c5e8442bd9cc9f6a02bf0711
Vinh <vinh@axcoto.com>
2020-05-09 00:31:08 -0700

    Add tmux quick setup

---
+++ tmux.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+session="bima"
+sessionexists=$(tmux ls | grep $session)
+
+if [ "$sessionexists" = "" ]; then
+  # Start New Session with our name
+  tmux new-session -d -s $session
+
+  tmux rename-window -t 0 'editor'
+  tmux send-keys -t 'editor' 'vim .'  C-m
+
+  tmux new-window -t ${session}:1 -n 'docker'
+  tmux send-keys -t 'docker' 'cd server/bima; docker-compose up' C-m
+else
+  tmux attach -t ${session}:0
+fi
